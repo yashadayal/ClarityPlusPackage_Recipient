83f56f68db6aa47fcbd0743778925674615e0685
Srivastava-Rani-Aakanksha <rani.aakanksha1999@gmail.com>
2023-04-22 19:51:20 +0530

    Minor Refactoring

--- src/main/java/com/ClarityPlusPackage/RecipientMService/Controller/RecipientDetailsController.java
+++ src/main/java/com/ClarityPlusPackage/RecipientMService/Controller/RecipientDetailsController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/recipient")
 public class RecipientDetailsController {
